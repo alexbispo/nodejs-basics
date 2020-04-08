28d6c00d2d590b083e3cf8945c2506d766643f53
Alex Bispo <alexbispo.artes@gmail.com>
2020-04-08 04:49:01 +0000

    Implements the challenge solution.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,65 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { url, title, techs } = request.body;
+
+  const newRepository = { id: uuid(), url, title, techs, likes: 0 };
+
+  repositories.push(newRepository);
+
+  return response.status(201).json(newRepository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { url, title, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex( r => r.id == id );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).end();
+  }
+
+ const { likes } = repositories[repositoryIndex];
+
+  const newRepository = { id, url, title, techs, likes };
+
+  repositories[repositoryIndex] = newRepository;
+
+  return response.json(newRepository);
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const { id } = req.params;
+
+  const repositoryIndex = repositories.findIndex( r => r.id == id );
+
+  if (repositoryIndex < 0) {
+    return res.status(400).end();
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return res.status(204).end();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex( r => r.id == id );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).end();
+  }
+
+  const repo =  repositories[repositoryIndex];
+  repo.likes++
+
+  return response.json(repo);
 });
 
 module.exports = app;
